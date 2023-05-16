799a64125bd870ae43d07207af5f5b460e6fdfe9
Jakub Szymański <jakubszymanski@hotmail.com>
2023-05-16 15:45:25 +0200

    added User class and repo

--- src/main/java/com/carrentalbackend/model/entity/User.java
+++ src/main/java/com/carrentalbackend/model/entity/User.java
@@ -1,11 +1,17 @@
 package com.carrentalbackend.model.entity;
 
-import jakarta.persistence.Entity;
-import jakarta.persistence.Id;
+import com.carrentalbackend.model.enumeration.Role;
+import jakarta.persistence.*;
 import lombok.AllArgsConstructor;
 import lombok.Builder;
 import lombok.Data;
 import lombok.NoArgsConstructor;
+import org.springframework.security.core.GrantedAuthority;
+import org.springframework.security.core.authority.SimpleGrantedAuthority;
+import org.springframework.security.core.userdetails.UserDetails;
+
+import java.util.Collection;
+import java.util.List;
 
 
 @Data
@@ -13,7 +19,43 @@ import lombok.NoArgsConstructor;
 @NoArgsConstructor
 @AllArgsConstructor
 @Entity
-public class User {
+//word "user" is restricted in some SQL dialects
+@Table(name="user_data")
+public class User implements UserDetails {
     @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
     private long id;
+    private String email;
+    private String password;
+    private Role role;
+
+    @Override
+    public Collection<? extends GrantedAuthority> getAuthorities() {
+        return List.of(new SimpleGrantedAuthority(role.name()));
+    }
+
+    @Override
+    public String getUsername() {
+        return email;
+    }
+
+    @Override
+    public boolean isAccountNonExpired() {
+        return true;
+    }
+
+    @Override
+    public boolean isAccountNonLocked() {
+        return true;
+    }
+
+    @Override
+    public boolean isCredentialsNonExpired() {
+        return true;
+    }
+
+    @Override
+    public boolean isEnabled() {
+        return true;
+    }
 }
