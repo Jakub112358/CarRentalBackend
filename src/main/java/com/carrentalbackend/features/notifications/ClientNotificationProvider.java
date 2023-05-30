42864f475ef1bf27cfe3110aa44d5502cc3eb27c
Jakub Szymański <jakubszymanski@hotmail.com>
2023-05-30 15:50:19 +0200

    ft_notifications: quick fix of example employee/client notifications

--- src/main/java/com/carrentalbackend/features/notifications/ClientNotificationProvider.java
+++ src/main/java/com/carrentalbackend/features/notifications/ClientNotificationProvider.java
@@ -1,11 +1,18 @@
 package com.carrentalbackend.features.notifications;
 
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Component;
+
 import java.util.ArrayList;
 import java.util.List;
 
+@Component
+@RequiredArgsConstructor
 public class ClientNotificationProvider {
     public List<NotificationDTO> getNotifications() {
-        return new ArrayList<>();
+        List<NotificationDTO> clientNotificationList = new ArrayList<>();
+        NotificationDTO notification = new NotificationDTO("example client title", "example client content");
+        clientNotificationList.add(notification);
+        return clientNotificationList;
     }
-
 }
--- src/main/java/com/carrentalbackend/features/notifications/EmployeeNotificationProvider.java
+++ src/main/java/com/carrentalbackend/features/notifications/EmployeeNotificationProvider.java
@@ -1,10 +1,17 @@
 package com.carrentalbackend.features.notifications;
 
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Component;
+
 import java.util.ArrayList;
 import java.util.List;
-
+@Component
+@RequiredArgsConstructor
 public class EmployeeNotificationProvider {
     public List<NotificationDTO> getNotifications() {
-        return new ArrayList<>();
+        List<NotificationDTO> clientNotificationList = new ArrayList<>();
+        NotificationDTO notification = new NotificationDTO("example employee title", "example employee content");
+        clientNotificationList.add(notification);
+        return clientNotificationList;
     }
 }
