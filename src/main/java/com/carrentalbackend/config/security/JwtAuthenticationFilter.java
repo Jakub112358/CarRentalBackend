fbef6aa983c70eb3792c6a0351946d8567a12e4a
Jakub Szymański <jakubszymanski@hotmail.com>
2023-06-06 13:55:55 +0200

    quick fix: catching expired jwt exception in filter

--- src/main/java/com/carrentalbackend/config/security/JwtAuthenticationFilter.java
+++ src/main/java/com/carrentalbackend/config/security/JwtAuthenticationFilter.java
@@ -1,5 +1,6 @@
 package com.carrentalbackend.config.security;
 
+import io.jsonwebtoken.ExpiredJwtException;
 import jakarta.servlet.FilterChain;
 import jakarta.servlet.ServletException;
 import jakarta.servlet.http.HttpServletRequest;
@@ -12,6 +13,7 @@ import org.springframework.security.core.userdetails.UserDetailsService;
 import org.springframework.security.web.authentication.WebAuthenticationDetailsSource;
 import org.springframework.stereotype.Component;
 import org.springframework.web.filter.OncePerRequestFilter;
+import org.springframework.web.servlet.HandlerExceptionResolver;
 
 import java.io.IOException;
 
@@ -20,6 +22,7 @@ import java.io.IOException;
 public class JwtAuthenticationFilter extends OncePerRequestFilter {
     private final JwtService jwtService;
     private final UserDetailsService userDetailsService;
+    private final HandlerExceptionResolver handlerExceptionResolver;
 
     @Override
     protected void doFilterInternal(HttpServletRequest request,
@@ -35,32 +38,36 @@ public class JwtAuthenticationFilter extends OncePerRequestFilter {
             filterChain.doFilter(request, response);
             return;
         }
+        try {
+            //substring with begin index 7 to remove "Bearer " part from the header;
+            jwt = authHeader.substring(7);
+            userEmail = jwtService.extractUsername(jwt);
 
-        //substring with begin index 7 to remove "Bearer " part from the header;
-        jwt = authHeader.substring(7);
-        userEmail = jwtService.extractUsername(jwt);
+            //checking if provided token contains email and if Security Context hasn't been already authenticated;
+            if (userEmail != null && SecurityContextHolder.getContext().getAuthentication() == null) {
+                //fetching UserDetails from Data Base;
+                UserDetails userDetails = userDetailsService.loadUserByUsername(userEmail);
 
-        //checking if provided token contains email and if Security Context hasn't been already authenticated;
-        if (userEmail != null && SecurityContextHolder.getContext().getAuthentication() == null) {
-            //fetching UserDetails from Data Base;
-            UserDetails userDetails = userDetailsService.loadUserByUsername(userEmail);
+                //checking if username from provided token matches corresponding username (user email in our case) in our database && if provided token isn't expired
+                if (jwtService.isTokenValid(jwt, userDetails)) {
+                    //creating object required by Security Context Holder to update our Security Context
+                    UsernamePasswordAuthenticationToken authToken = new UsernamePasswordAuthenticationToken(
+                            userDetails,
+                            null,
+                            userDetails.getAuthorities()
 
-            //checking if username from provided token matches corresponding username (user email in our case) in our database && if provided token isn't expired
-            if (jwtService.isTokenValid(jwt, userDetails)) {
-                //creating object required by Security Context Holder to update our Security Context
-                UsernamePasswordAuthenticationToken authToken = new UsernamePasswordAuthenticationToken(
-                        userDetails,
-                        null,
-                        userDetails.getAuthorities()
-
-                );
-                authToken.setDetails(
-                        new WebAuthenticationDetailsSource().buildDetails(request)
-                );
-                //updating Security Context
-                SecurityContextHolder.getContext().setAuthentication(authToken);
+                    );
+                    authToken.setDetails(
+                            new WebAuthenticationDetailsSource().buildDetails(request)
+                    );
+                    //updating Security Context
+                    SecurityContextHolder.getContext().setAuthentication(authToken);
+                }
             }
+
+            filterChain.doFilter(request, response);
+        } catch (ExpiredJwtException e) {
+            handlerExceptionResolver.resolveException(request, response, null, e);
         }
-        filterChain.doFilter(request, response);
     }
 }
